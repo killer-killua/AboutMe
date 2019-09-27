2d04f2191327f61db7d234630fe20435b6cf2e10
Justice Thomas <justicethomas05@gmail.com>
2019-09-26 18:26:49 -0700

    made it so when button is pushed it will display user typed nickname

--- app/src/main/java/com/example/aboutme/MainActivity.kt
+++ app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -2,11 +2,34 @@ package com.example.aboutme
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    lateintit var editText: EditText
+
+    lateinit var nicknameTextView: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.done_button).setOnClickListener {
+            addNickname(it)
+        }
+
+        editText = findViewById(R.id.nickname_edit)
+        TetxView = findViewById(R.id.nickname_txt)
+    }
+
+    private fun addNickname(view: View){
+
+        nicknameTextView.text = editText.text
+        edittext.visibility = View.GONE
+        view.visibility = View.GONE
+
     }
 }
