bd098eb88b8a8694a0dddbbbf285861bf191b194
Justice Thomas <justicethomas05@gmail.com>
2019-10-01 13:01:23 -0700

    added another textView class under the button and data binded the button and the edittext and textview views.

--- app/src/main/java/com/example/aboutme/MainActivity.kt
+++ app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -6,31 +6,47 @@ import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import com.example.aboutme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    lateintit var editText: EditText
+    private lateinit var binding: ActivityMainBinding
+
+    lateinit var editText: EditText
 
     lateinit var nicknameTextView: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        findViewById<Button>(R.id.done_button).setOnClickListener {
+        //findViewById<Button>(R.id.done_button).setOnClickListener {
+            //addNickname(it)
+
+        binding.doneButton.setOnClickListener {
+
             addNickname(it)
         }
+        //}
 
         editText = findViewById(R.id.nickname_edit)
-        TetxView = findViewById(R.id.nickname_txt)
+        TextView = findViewById(R.id.nickname_txt)
     }
 
     private fun addNickname(view: View){
+        binding.apply {
+            nicknameTxt.text = nicknameEdit.text
+            nicknameEdit.visibility = View.GONE
+            doneButton.visibility = View.GONE
+            nicknameTxt.visibility = View.VISIBLE
+        }
 
-        nicknameTextView.text = editText.text
-        edittext.visibility = View.GONE
-        view.visibility = View.GONE
-        nicknameTextView.visibility = View.VISIBLE
+        //nicknameTextView.text = editText.text
+        //editText.visibility = View.GONE
+        //view.visibility = View.GONE
+        //nicknameTextView.visibility = View.VISIBLE
 
     }
 }
