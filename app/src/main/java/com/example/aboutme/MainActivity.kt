07ac7362712f1d894961337a54f39c25da6ee955
Justice Thomas <justicethomas05@gmail.com>
2019-10-01 14:09:39 -0700

    added a data class and made changes to text view and  addNickName class to make the app more efficient

--- app/src/main/java/com/example/aboutme/MainActivity.kt
+++ app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -13,14 +13,18 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
-    lateinit var editText: EditText
+    private val myName: MyName = MyName("Justice")
 
-    lateinit var nicknameTextView: TextView
+    //lateinit var editText: EditText
+
+    //lateinit var nicknameTextView: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //setContentView(R.layout.activity_main)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.myName = myName
 
         //findViewById<Button>(R.id.done_button).setOnClickListener {
             //addNickname(it)
@@ -31,13 +35,15 @@ class MainActivity : AppCompatActivity() {
         }
         //}
 
-        editText = findViewById(R.id.nickname_edit)
-        TextView = findViewById(R.id.nickname_txt)
+        //editText = findViewById(R.id.nickname_edit)
+        //TextView = findViewById(R.id.nickname_txt)
     }
 
     private fun addNickname(view: View){
         binding.apply {
-            nicknameTxt.text = nicknameEdit.text
+            //nicknameTxt.text = nicknameEdit.text
+            myName?.nickname = nicknameEdit.text.toString()
+            invalidateAll()
             nicknameEdit.visibility = View.GONE
             doneButton.visibility = View.GONE
             nicknameTxt.visibility = View.VISIBLE
